21dfab6b59a6d451fd9bd1e083cd395350cb38af
justvanz <vpbiaca@usm.edu.ph>
2024-10-14 18:08:24 +0800

    first commit

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>My Products</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.1/font/bootstrap-icons.css">
+    <link rel="stylesheet" href="resources/css/app.css">
+</head>
+<body>   
+
+    <div class="container col-md-8">
+        <h3 class=" mt-3"> Product Management</h3>
+            @yield('content')
+            {{-- <div>
+                <button><a href="{{ route('products.index') }}">Get Started</a>
+                </button>
+            </div> --}}
+
+    </div>
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
+</body>
+</html>
---
+++ routes/web.php
@@ -0,0 +1,7 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProductController;
+Route::get('/', [ProductController::class, 'index'])->name('products.index');
+
+Route::resource('products', ProductController::class);
